81185502569238f906cb65887aa5652a0036a52f
dnwls6102 <dnwls6102@naver.com>
2024-11-04 22:52:20 +0900

    컴파일 오류 수정

--- threads/thread.c
+++ threads/thread.c
@@ -124,7 +124,7 @@ thread_init (void) {
 	lock_init (&tid_lock);
 	list_init (&ready_list);
     list_init(&sleep_list);  // 슬립 큐 초기화
-    tick = INT64_MAX;        // tick을 최대값으로 초기화
+    next_tick_to_awake = INT64_MAX;        // tick을 최대값으로 초기화
 	list_init (&destruction_req);
 
 	/* Set up a thread structure for the running thread. */
@@ -331,11 +331,11 @@ thread_sleep(int64_t wakeup_tick) {
     curr->wakeup_tick = wakeup_tick;  // 스레드의 wakeup_tick 설정
 
     /* 슬립 큐에 시간순으로 정렬하여 삽입 */
-    list_insert_ordered(&sleep_list, &curr->elem, cmp_wakeup_tick, NULL);
+    list_insert_ordered(&sleep_list, &curr->elem, wakeup_tick_less, NULL);
 
     /* 글로벌 tick 업데이트 */
-    if (tick > wakeup_tick) {
-        tick = wakeup_tick;
+    if (next_tick_to_awake > wakeup_tick) {
+        next_tick_to_awake = wakeup_tick;
     }
 
     thread_block();  // 스레드를 블록 상태로 전환
@@ -354,13 +354,13 @@ thread_wake(int64_t current_ticks) {
     while (!list_empty(&sleep_list)) {
         e = list_front(&sleep_list);
         struct thread *t = list_entry(e, struct thread, elem);
-        if (t->wakeup_time <= current_ticks) {
+        if (t->wakeup_tick <= current_ticks) {
             /* 깨울 시간이라면 스레드를 깨움 */
             list_pop_front(&sleep_list);
             thread_unblock(t);
         } else {
             /* 슬립 리스트가 정렬되어 있으므로 더 이상 확인할 필요가 없음*/
-            next_tick_to_awake = t->wakeup_time;
+            next_tick_to_awake = t->wakeup_tick;
             break;
         }
     }
